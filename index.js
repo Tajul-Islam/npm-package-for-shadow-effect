d4ddd45decb127b12d02775ac2442fdc44e22032
Tajul Islam <tajul@jotno.net>
2021-04-11 17:38:57 +0600

    update border style

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ function shadowizard(options) {
     } else {
         options.shadow_type = '15px'
     }
-    if(options.border_radius === 'round') {
-        options.borderRadius = '8px'
-    }
-    if(options.border_radius === 'circle') {
-        options.borderRadius = '50%'
-    }
     images.forEach(image => {
         image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`
         if(options.padding) {
             image.style.padding = '1em';
+        }
+        if(options.border_radius === 'round') {
+            image.style.borderRadius = '8px'
+        }
+        if(options.border_radius === 'circle') {
+            image.style.borderRadius = '50%'
         }
     })
 }
