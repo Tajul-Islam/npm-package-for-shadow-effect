c81f81582caf78e6ad2ec3e7285047fc58778e4e
Tajul Islam <tajul@jotno.net>
2021-04-11 17:34:48 +0600

    add border radius

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function shadowizard(options) {
     let images  = document.querySelectorAll('.shadowizard');
-    if(options.shadow_type === ' hard') {
+    if(options.shadow_type === 'hard') {
         options.shadow_type = '0px'
     } else {
         options.shadow_type = '15px'
+    }
+    if(options.border_radius === 'round') {
+        options.borderRadius = '8px'
+    }
+    if(options.border_radius === 'circle') {
+        options.borderRadius = '50%'
     }
     images.forEach(image => {
         image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`
